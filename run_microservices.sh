34096da480c0bb684374038998efa1e49bf7af69
Armin S <arminnessie@gmail.com>
2024-04-06 17:14:28 +0200

    Update run_microservices.sh

--- run_microservices.sh
+++ run_microservices.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file="./0_place_dataset_csv_here/dataset.csv"
 if [ ! -e "$file" ]; then
-    echo "Error: dataset.csv not found. Please download it from https://www.kaggle.com/datasets/andrewmvd/steam-reviews and unzip it in '1_place_dataset_csv_here'."
+    echo "Error: dataset.csv not found. Please download it from https://www.kaggle.com/datasets/andrewmvd/steam-reviews and unzip it in '0_place_dataset_csv_here'."
     exit
 fi
 echo "Continue..."
