29bffac74f67c6128f76aa0cd56574238eec5e0c
Armin S <arminnessie@gmail.com>
2024-04-27 21:58:30 +0200

    Update run_microservices.sh

--- run_microservices.sh
+++ run_microservices.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 file="./0_place_dataset_csv_here/dataset.csv"
 if [ ! -e "$file" ]; then
-    echo "Error: dataset.csv not found. Please download it from https://www.kaggle.com/datasets/andrewmvd/steam-reviews and unzip it in '0_place_dataset_csv_here'."
+    echo "Error: dataset.csv not found. Please download it from https://www.kaggle.com/datasets/andrewmvd/steam-reviews and unzip it in '1_place_dataset_csv_here'."
     exit
 fi
-echo "Continue..."
+
+# 1_process_dataset
+chmod +x 1_process_dataset/script.py
+docker build -t step-1 1_process_dataset
+docker run -v "$(pwd)":/app step-1
